ebcc28a690f9418a2bb73b9029250583c6b0c617
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-01-15 12:16:27 -0600

    RNAP13 first

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>RNAP13</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D4D945',
     alignItems: 'center',
     justifyContent: 'center',
   },
